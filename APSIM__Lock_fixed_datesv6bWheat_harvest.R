b1917f0e5eac905785860a242514cc58728ee326
Ouzman <ouz001@csiro.au>
2025-10-07 17:45:52 +1100

    for meeting now with just harvest outputs

--- APSIM__Lock_fixed_datesv6bWheat_harvest.R
+++ APSIM__Lock_fixed_datesv6bWheat_harvest.R
@@ -4,137 +4,111 @@ library(readr)
 library(stringr)
 library(lubridate)
 library(readxl)
-library(DBI)
-library(RSQLite)
-
-#on virtual machine its N not D
-file_path1  <- "N:/work/Riskwise/early_sowing/"
-
-#file_path1  <- "D:/work/Riskwise/early_sowing/"
-site        <- "Lock"
-file_path2  <- "/APSIM_runs/Wheat/"
-file_name   <- "Lock_fixed_Pengcheng_HF_rule at harvest"
-# "D:\work\RiskWise\early_sowing\Lock\APSIM_runs\Wheat\Lock_fixed_Pengcheng_HF_rule.db"
-con <- dbConnect(SQLite(),  "N:/work/RiskWise/early_sowing/Lock/APSIM_runs/Wheat/Lock_fixed_Pengcheng_HF_rule at harvest.db")
-dbListTables(con)
-data <- dbGetQuery(con, "SELECT * FROM HarvestReport")
-dbDisconnect(con)
-rm(con)
 
 
-names(data)
-
-Early_sowing_Lock <-data
-rm(data)
-
-
-
-   
-
-
-Decile_18046 <- read_csv(paste0(
-  file_path1,
-  site,
-  "/Deciles/",
-  "deciles per year_Lock18046.csv"))
-  
-
-str(Early_sowing_Lock)
-str(Decile_18046)
-
-### get year from date clm
-
-Early_sowing_Lock <- Early_sowing_Lock %>%
-  mutate(year = year(Clock.Today))
-#create a sowing date clm
-Early_sowing_Lock <- Early_sowing_Lock %>% 
-  mutate(sowing_date1 = paste0(StartDate,"-", year)) %>% 
-  mutate(sowing_date = as.Date(sowing_date1,format = "%d-%B-%Y") ) %>% 
-  select(-sowing_date1)
-
-check <- Early_sowing_Lock %>% select( Clock.Today,  StartDate,year, sowing_date )
-str(check)
-Check_max_date <- max( check$sowing_date   )
-Check_max_date #should be 2024 -07-31
-rm( Check_max_date,check )
-
-### merge the data
-
-Early_sowing_Lock_met_decile <- left_join(Early_sowing_Lock, Decile_18046, by = "year")
-str(Early_sowing_Lock_met_decile)
-
-
-
-
-
-### get the harvest rows only
-Early_sowing_Lock_met_decile <- Early_sowing_Lock_met_decile %>% 
-   #filter(Wheat.Phenology.CurrentStageName == "HarvestRipe") %>% 
-   select(year, StartDate, decile, 
-          Yield, FrostHeatDamageFunctions.FrostHeatYield, 
-          Wheat.Phenology.CurrentStageName, 
-          Clock.Today, 
-          sowing_date ,
-          InCropRainfall,
-          AllYearRainfall,
-          FrostHeatDamageFunctions.FrostEventNumber,
-          InCropMeanSoilWater) %>% 
-  rename(harvest_date = Clock.Today,
-         FrostHeatYield = FrostHeatDamageFunctions.FrostHeatYield) %>% 
-  select(-Wheat.Phenology.CurrentStageName)
-
-
-
-
-
-
-
-
-
-
-
-## before writing out the un summaries data with germination date. Just add a few things to help plot
-str(Early_sowing_Lock_met_decile)
-
-Early_sowing_Lock_met_decile$harvest_date <- as.Date(Early_sowing_Lock_met_decile$harvest_date)
-
-Early_sowing_Lock_met_decile <- Early_sowing_Lock_met_decile %>% 
-  mutate(
-         DOY_sowing =      lubridate::yday( sowing_date),
-         DOY_harvest =     lubridate::yday( harvest_date),
-         CommonDate_sowing = ymd(paste0("2000-",str_sub(as.character(sowing_date),-5))),
-         CommonDate_harvest = ymd(paste0("2000-",str_sub(as.character(harvest_date),-5))),
-         )
-
-           
-           
-write_csv(Early_sowing_Lock_met_decile, 
-          file =paste0(
+Early_sowing_Lock_met_decile_wheat <- read_csv(paste0(
             "N:/work/Riskwise/early_sowing/Lock/For_Decile_cal/Wheat/",
             "/APSIM_yld_Decile_Lock18046_No_summary", ".csv"))
+           
+           
+Early_sowing_Lock_met_decile_wheat <- Early_sowing_Lock_met_decile_wheat %>% 
+  mutate( Yield =  Yield/10)
 
 
+
+
+#### some info on frost days
+str(Early_sowing_Lock_met_decile_wheat)
+Ave_Sum_frost_days_sowing_date <- Early_sowing_Lock_met_decile_wheat %>% 
+  group_by(CommonDate_sowing) %>% 
+  summarise(
+    av_frost_days = mean(FrostHeatDamageFunctions.FrostEventNumber, na.rm = TRUE),
+    sum_frost_days = sum(FrostHeatDamageFunctions.FrostEventNumber, na.rm = TRUE)
+  )
+
+Ave_Sum_frost_days_sowing_date
+
+plot4_point <- Ave_Sum_frost_days_sowing_date %>% 
+  ggplot(aes(x = CommonDate_sowing,  av_frost_days))+
+  geom_point()+
+  theme_classic()+
+  theme(axis.text.x = element_text(angle = 90, vjust = 1, hjust=1))+
+  labs(title = "Mean Number frost days vs early sowing dates Lock 18046.\nMust sow rule.",
+       subtitle = "Wheat crop, unadjusted climate file",
+       y = "Mean number of frost days for Sowing Date",
+       x ="Sowing date common year"
+  )
+plot4_point
+
+plot5_point <- Ave_Sum_frost_days_sowing_date %>% 
+  ggplot(aes(x = CommonDate_sowing,  sum_frost_days))+
+  geom_point()+
+  theme_classic()+
+  theme(axis.text.x = element_text(angle = 90, vjust = 1, hjust=1))+
+  labs(title = "Sum Number frost days vs early sowing dates Lock 18046.\nMust sow rule.",
+       subtitle = "Wheat crop, unadjusted climate file",
+       y = "Sum number of frost days for Sowing Date",
+       x ="Sowing date common year"
+  )
+plot5_point
+
+
+
+ggsave(plot = plot4_point,
+       filename = paste0(path_saved_files,"/Mean_no_FrostDaysvs_Sowing_dates_Lock_Wheat", ".png" ),
+       width = 20, height = 12, units = "cm")
+
+ggsave(plot = plot5_point,
+       filename = paste0(path_saved_files,"/Sum_no_FrostDaysvs_Sowing_dates_Lock_Wheat", ".png" ),
+       width = 20, height = 12, units = "cm")
+
+
+### for sowing date 13-May
+
+Bonnie_mean_sowing_date_Sum_frost_days <- Early_sowing_Lock_met_decile_wheat %>% 
+  filter(CommonDate_sowing == "2000-05-13") %>% 
+  group_by(CommonDate_sowing) %>% 
+  summarise(
+    count_years = n(),
+    av_frost_days = mean(FrostHeatDamageFunctions.FrostEventNumber, na.rm = TRUE),
+    sum_frost_days = sum(FrostHeatDamageFunctions.FrostEventNumber, na.rm = TRUE)
+  )
+
+Bonnie_mean_sowing_date_Sum_frost_days
 ###############################################################################
 ## Plots ###
-str(ES_Lock_met_decile_harvest_germ)
+str(Early_sowing_Lock_met_decile_wheat)
+
+
 
 path_saved_files <- file_path_input_data<-file.path("N:","work", "Riskwise", "early_sowing", "Lock", "Results" , "Wheat")
 
+Early_sowing_Lock_met_decile_wheat_long <- Early_sowing_Lock_met_decile_wheat %>% 
+  pivot_longer(
+    cols = contains("yield"), # Select columns to pivot
+    names_to = "yield_Type",     # Name of the new column for original column names
+    values_to = "yield_Value"    # Name of the new column for values
+  )
+
+
+
+
 # non adjusted yld
-plot1_box <- ES_Lock_met_decile_harvest_germ %>% 
-  ggplot(aes(x =as.factor(DOY_sowing), Yield), group)+
+plot1_box <- Early_sowing_Lock_met_decile_wheat_long %>% 
+  ggplot(aes(x =DOY_sowing, yield_Value, group = DOY_sowing))+
   geom_boxplot()+
   #geom_point(alpha =0.2)+
-  geom_point(data= ES_Lock_met_decile_harvest_germ %>% filter(year == 2023), 
-             aes(x =as.factor(DOY_sowing), Yield), colour ="red")+
+  geom_point(data= Early_sowing_Lock_met_decile_wheat_long %>% filter(year == 2023), 
+             aes(x =DOY_sowing, yield_Value), colour ="red")+
   theme_classic()+
   theme(legend.position = "none",
         axis.text.x = element_text(angle = 90, vjust = 1, hjust=1),
         panel.border = element_rect(colour = "blue", fill=NA, linewidth=1))+
+  facet_wrap(.~yield_Type)+
   
   #ylim(0,600)+
-  labs(title = "Yield vs early sowing dates Lock 18046.\nMust sow rule. No frost rule used or penalty applied",
-       subtitle = "Wheat crop, unadjusted climate file",
+  labs(title = "Yield vs early sowing dates Lock 18046.\nMust sow rule.",
+       subtitle = "Wheat crop, unadjusted climate file. Irrigation at sowing",
        y = "Yield",
        x ="Sowing date expressed as DOY",
        caption = "Red dot = 2023."
@@ -147,49 +121,31 @@ ggsave(plot = plot1_box,
        width = 20, height = 12, units = "cm")
 
 ################################################################################
-## adjusted yld
-plot2_box <- ES_Lock_met_decile_harvest_germ %>% 
-  ggplot(aes(x =as.factor(DOY_sowing),  FrostHeatYield), group)+
-  geom_boxplot()+
-  #geom_point(alpha =0.2)+
-  geom_point(data= ES_Lock_met_decile_harvest_germ %>% filter(year == 2023), 
-             aes(x =as.factor(DOY_sowing), Yield), colour ="red")+
-  theme_classic()+
-  theme(legend.position = "none",
-        axis.text.x = element_text(angle = 90, vjust = 1, hjust=1),
-        panel.border = element_rect(colour = "blue", fill=NA, linewidth=1))+
-  
-  #ylim(0,600)+
-  labs(title = "Yield vs early sowing dates Lock 18046.\nMust sow rule. FrostHeatDamageFunctions penalty applied",
-       subtitle = "Wheat crop, unadjusted climate file",
-       y = "Yield",
-       x ="Sowing date expressed as DOY",
-       caption = "Red dot = 2023."
-  )
-plot2_box
 
-
-ggsave(plot = plot2_box,
-       filename = paste0(path_saved_files,"/Box_Frostyield_vs_Sowing_dates_Lock_Wheat", ".png" ),
-       width = 20, height = 12, units = "cm")
 
 
 
 ################################################################################
 ## both
-str(ES_Lock_met_decile_harvest_germ)
-ES_Lock_met_decile_harvest_germ_summary <- ES_Lock_met_decile_harvest_germ %>% 
+str(Early_sowing_Lock_met_decile_wheat)
+Early_sowing_Lock_met_decile_wheat_summary <- Early_sowing_Lock_met_decile_wheat %>% 
   group_by(DOY_sowing) %>% 
   summarise(Yield_mean = mean(Yield, na.rm = TRUE),
             FrostHeatYield_mean = mean(FrostHeatYield, na.rm = TRUE))
-str(ES_Lock_met_decile_harvest_germ_summary)
+str(Early_sowing_Lock_met_decile_wheat_summary)
+
+Early_sowing_Lock_met_decile_wheat_summary_long <- Early_sowing_Lock_met_decile_wheat_summary %>% 
+  pivot_longer(
+    cols = contains("yield"), # Select columns to pivot
+    names_to = "yield_Type",     # Name of the new column for original column names
+    values_to = "yield_Value"    # Name of the new column for values
+  )
 
 
-plot3_point <- ES_Lock_met_decile_harvest_germ_summary %>% 
-  ggplot(aes(x =as.factor(DOY_sowing),  Yield_mean))+
+
+plot3_point <- Early_sowing_Lock_met_decile_wheat_summary_long %>% 
+  ggplot(aes(x =DOY_sowing,  yield_Value, colour = yield_Type))+
   geom_point()+
-  geom_point(data= ES_Lock_met_decile_harvest_germ_summary, 
-             aes(x =as.factor(DOY_sowing), FrostHeatYield_mean), colour ="blue")+
   theme_classic()+
   theme(axis.text.x = element_text(angle = 90, vjust = 1, hjust=1))+
   
@@ -197,8 +153,7 @@ plot3_point <- ES_Lock_met_decile_harvest_germ_summary %>%
   labs(title = "Mean Yield vs early sowing dates Lock 18046.\nMust sow rule. \nYield and Frost Damage Functions Yield",
        subtitle = "Wheat crop, unadjusted climate file",
        y = "Mean Yield for Sowing Date",
-       x ="Sowing date expressed as DOY",
-       caption = "Blue = Mean Frost Yield, Black = Mean Yield"
+       x ="Sowing date expressed as DOY"
   )
 plot3_point
 ggsave(plot = plot3_point,
@@ -210,18 +165,11 @@ ggsave(plot = plot3_point,
 ## histogram
 str(ES_Lock_met_decile_harvest_germ)
 
-## make data long first 
-ES_Lock_met_decile_harvest_germ_long <- ES_Lock_met_decile_harvest_germ %>%
-  pivot_longer(
-    cols = contains("yield"), # Select columns to pivot
-    names_to = "yield_Type",     # Name of the new column for original column names
-    values_to = "yield_Value"    # Name of the new column for values
-  )
 
-str(ES_Lock_met_decile_harvest_germ_long)
+str(Early_sowing_Lock_met_decile_wheat_long)
 
 
-plot1_hisrogram <-  ggplot(ES_Lock_met_decile_harvest_germ_long, aes(x=yield_Value, color=yield_Type, fill = yield_Type)) +
+plot1_hisrogram <-  ggplot(Early_sowing_Lock_met_decile_wheat_long, aes(x=yield_Value, color=yield_Type, fill = yield_Type)) +
   geom_histogram( alpha=0.2, position="identity")+
   labs(title = "Lock Histogram Wheat Yield - Frost adjustment and No adjustment",
        subtitle = "Climate file not adjusted",
@@ -232,7 +180,7 @@ plot1_hisrogram <-  ggplot(ES_Lock_met_decile_harvest_germ_long, aes(x=yield_Val
 plot1_hisrogram
 
 
-plot2_hisrogram <-  ggplot(ES_Lock_met_decile_harvest_germ_long, aes(x=yield_Value,color=yield_Type, fill = yield_Type)) +
+plot2_hisrogram <-  ggplot(Early_sowing_Lock_met_decile_wheat_long, aes(x=yield_Value,color=yield_Type, fill = yield_Type)) +
   geom_histogram( )+
   labs(title = "Lock Histogram Wheat Yield - Frost adjustment and No adjustment",
        subtitle = "Climate file not adjusted",
